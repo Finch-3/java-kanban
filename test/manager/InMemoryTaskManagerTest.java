6bc33f638dde8b43d3577bbdec90c3367a91d874
Finch <finch-3@ya.ru>
2025-02-17 22:26:44 +0300

    Initial commit

--- test/manager/InMemoryTaskManagerTest.java
+++ test/manager/InMemoryTaskManagerTest.java
@@ -19,10 +19,9 @@ class InMemoryTaskManagerTest {
 
     Task task = new Task("Задача", "Описание");
     Epic epic = new Epic("Эпик", "Описание эпика");
-    Subtask subtask = new Subtask(1, "Подзадача", "Описание подзадачи");
 
     @Test
-    void addGetPrintTask() {
+    void addGetPrintDeleteTask() {
         final int taskId = taskManager.addTask(task);
 
         final Task savedTask = taskManager.getTaskById(taskId);
@@ -35,10 +34,36 @@ class InMemoryTaskManagerTest {
         assertNotNull(tasks, "Задачи не возвращаются.");
         assertEquals(1, tasks.size(), "Неверное количество задач.");
         assertEquals(task, tasks.get(0), "Задачи не совпадают.");
+
+        taskManager.deleteTaskById(taskId);
+
+        assertNull(taskManager.getTaskById(taskId));
+        assertEquals(0, taskManager.getTasks().size(), "Неверное количество задач.");
     }
 
     @Test
-    void addGetPrintEpic() {
+    void updateDeleteListTask() {
+        Task updateTask = new Task(taskManager.addTask(task), "Задача", "Описание", Status.IN_PROGRESS);
+        final int taskId = taskManager.updateTask(updateTask);
+
+        final Task savedTask = taskManager.getTaskById(taskId);
+
+        assertNotNull(savedTask, "Задача не найдена.");
+        assertEquals(updateTask, savedTask, "Задачи не совпадают.");
+
+        final ArrayList<Task> tasks = taskManager.getTasks();
+
+        assertNotNull(tasks, "Задачи не возвращаются.");
+        assertEquals(1, tasks.size(), "Неверное количество задач.");
+        assertEquals(updateTask, tasks.get(0), "Задачи не совпадают.");
+
+        taskManager.clearTask();
+
+        assertEquals(0, taskManager.getTasks().size(), "Неверное количество задач.");
+    }
+
+    @Test
+    void addGetPrintDeleteEpic() {
         final int epicId = taskManager.addEpic(epic);
 
         final Epic savedEpic = taskManager.getEpicById(epicId);
@@ -51,11 +76,38 @@ class InMemoryTaskManagerTest {
         assertNotNull(epics, "Задачи не возвращаются.");
         assertEquals(1, epics.size(), "Неверное количество задач.");
         assertEquals(epic, epics.get(0), "Задачи не совпадают.");
+
+        taskManager.deleteEpicById(epicId);
+
+        assertNull(taskManager.getEpicById(epicId));
+        assertEquals(0, taskManager.getEpics().size(), "Неверное количество задач.");
     }
 
     @Test
-    void addGetPrintSubtask() {
-        taskManager.addEpic(epic);
+    void updateDeleteListEpic() {
+        Epic updateEpic = new Epic(taskManager.addEpic(epic), "Эпик", "Описание");
+        final int epicId = taskManager.updateEpic(updateEpic);
+
+        final Epic savedEpic = taskManager.getEpicById(epicId);
+
+        assertNotNull(savedEpic, "Задача не найдена.");
+        assertEquals(updateEpic, savedEpic, "Задачи не совпадают.");
+
+        final ArrayList<Epic> epics = taskManager.getEpics();
+
+        assertNotNull(epics, "Задачи не возвращаются.");
+        assertEquals(1, epics.size(), "Неверное количество задач.");
+        assertEquals(updateEpic, epics.get(0), "Задачи не совпадают.");
+
+        taskManager.clearEpics();
+
+        assertEquals(0, taskManager.getEpics().size(), "Неверное количество задач.");
+    }
+
+    @Test
+    void addGetPrintDeleteSubtask() {
+        Subtask subtask = new Subtask(taskManager.addEpic(epic), "Подзадача", "Описание подзадачи");
+
         final int subtaskId = taskManager.addSubtask(subtask);
 
         final Subtask savedSubtask = taskManager.getSubtaskById(subtaskId);
@@ -68,7 +120,35 @@ class InMemoryTaskManagerTest {
         assertNotNull(subtasks, "Задачи не возвращаются.");
         assertEquals(1, subtasks.size(), "Неверное количество задач.");
         assertEquals(subtask, subtasks.get(0), "Задачи не совпадают.");
+
+        taskManager.deleteSubtaskById(subtaskId);
+
+        assertNull(taskManager.getSubtaskById(subtaskId));
+        assertEquals(0, taskManager.getSubtasks().size(), "Неверное количество задач.");
     }
 
+    @Test
+    void updateDeleteListSubtask() {
+        Subtask subtask = new Subtask(taskManager.addEpic(epic), "Подзадача", "Описание подзадачи");
+        Subtask updateSubtask = new Subtask(taskManager.addSubtask(subtask), "Подзадача",
+                "Описание", Status.IN_PROGRESS);
+        final int subtaskId = taskManager.updateSubtask(updateSubtask);
+
+        final Subtask savedSubtask = taskManager.getSubtaskById(subtaskId);
+
+        assertNotNull(savedSubtask, "Задача не найдена.");
+        assertEquals(updateSubtask, savedSubtask, "Задачи не совпадают.");
+
+        final ArrayList<Subtask> subtasks = taskManager.getSubtasks();
+
+        assertNotNull(subtasks, "Задачи не возвращаются.");
+        assertEquals(1, subtasks.size(), "Неверное количество задач.");
+        assertEquals(updateSubtask, subtasks.get(0), "Задачи не совпадают.");
+
+        taskManager.clearSubtask();
+
+        assertEquals(0, taskManager.getSubtasks().size(), "Неверное количество задач.");
+        assertEquals(0, epic.getSubtaskList().size(), "Неверное количество задач.");
+    }
 
 }
