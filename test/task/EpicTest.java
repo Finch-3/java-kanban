db1b353ca8fbd26a54c7fde9e91e04b43bc5f732
Finch <finch-3@ya.ru>
2025-03-10 17:11:44 +0300

    Initial commit

--- test/task/EpicTest.java
+++ test/task/EpicTest.java
@@ -1,5 +1,7 @@
 package task;
 
+import manager.InMemoryTaskManager;
+import manager.TaskManager;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -14,5 +16,14 @@ class EpicTest {
         epic2.setId(1);
 
         Assertions.assertEquals(epic1, epic2, "Задачи не равны");
+    }
+    @Test
+    public void ThereIsNoCollusionInId() {
+        TaskManager manager = new InMemoryTaskManager();
+        Epic epic1 = new Epic("Задача 1", "Описание 1");
+        manager.addEpic(epic1);
+        Epic epic2 = new Epic("Задача 2", "Описание 2");
+        manager.addEpic(epic2);
+        Assertions.assertNotEquals(epic1, epic2, "Задачи не равны");
     }
 }
