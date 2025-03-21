f4553527a4b9aaab89da44b2335a3ca7302cfc7b
Finch <finch-3@ya.ru>
2025-03-21 21:14:40 +0300

    improved the history manager's tests

--- test/manager/InMemoryHistoryManagerTest.java
+++ test/manager/InMemoryHistoryManagerTest.java
@@ -6,6 +6,8 @@ import task.Epic;
 import task.Subtask;
 import task.Task;
 
+import java.util.LinkedHashSet;
+
 class InMemoryHistoryManagerTest {
 
     TaskManager manager = Manager.getDefault();
@@ -13,30 +15,62 @@ class InMemoryHistoryManagerTest {
     @Test
     void addHistoryTaskEpicSubtaskNoRepeatDelete() {
         Task task = new Task("Задача", "Описание");
-        Epic epic = new Epic("Эпик", "Описание эпика");
+        Task task1 = new Task("Задача 1", "Описание 1");
+
         manager.addTask(task);
+        manager.addTask(task1);
+
+        Epic epic = new Epic("Эпик", "Описание эпика");
+        Epic epic1 = new Epic("Эпик 1", "Описание эпика 1");
+
         manager.addEpic(epic);
+        manager.addEpic(epic1);
 
         Subtask subtask = new Subtask(epic.getId(), "Подзадача", "Описание подзадачи");
+        Subtask subtask1 = new Subtask(epic.getId(), "Подзадача 1", "Описание подзадачи 1");
+        Subtask subtask2 = new Subtask(epic.getId(), "Подзадача 2", "Описание подзадачи 2");
 
         manager.addSubtask(subtask);
+        manager.addSubtask(subtask1);
+        manager.addSubtask(subtask2);
 
         manager.getTaskById(task.getId());
+        manager.getTaskById(task1.getId());
+
+        manager.getEpicById(epic.getId());
+        manager.getEpicById(epic1.getId());
+
+        manager.getSubtaskById(subtask.getId());
+        manager.getSubtaskById(subtask1.getId());
+        manager.getSubtaskById(subtask2.getId());
+
+        Assertions.assertEquals(7, new LinkedHashSet<>(manager.getHistory()).size(),
+                "Задачи повторяются");
+
         manager.getTaskById(task.getId());
         manager.getEpicById(epic.getId());
-        manager.getEpicById(epic.getId());
-        manager.getSubtaskById(subtask.getId());
-        manager.getSubtaskById(subtask.getId());
 
-        Assertions.assertEquals(3, manager.getHistory().size(), "Задачи повторяются");
+        manager.getTaskById(task1.getId());
+        manager.getEpicById(epic1.getId());
+
+        manager.getSubtaskById(subtask1.getId());
+        manager.getSubtaskById(subtask.getId());
+        manager.getSubtaskById(subtask2.getId());
+
+        Assertions.assertEquals(7, new LinkedHashSet<>(manager.getHistory()).size(),
+                "Задачи повторяются");
 
         manager.deleteTaskById(task.getId());
 
-        Assertions.assertEquals(2, manager.getHistory().size(), "Задача не удалена");
+        Assertions.assertFalse(manager.getHistory().contains(task));
 
         manager.deleteEpicById(epic.getId());
 
-        Assertions.assertEquals(0, manager.getHistory().size(), "Задача не удалена");
+        Assertions.assertFalse(manager.getHistory().contains(epic));
+        Assertions.assertFalse(manager.getHistory().contains(subtask));
+        Assertions.assertFalse(manager.getHistory().contains(subtask1));
+        Assertions.assertFalse(manager.getHistory().contains(subtask2));
 
+        Assertions.assertEquals(2, manager.getHistory().size(), "Задачи не удалились");
     }
 }
